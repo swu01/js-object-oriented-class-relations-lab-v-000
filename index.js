57825cb64e58a5e787888faa8cae1930fcb67a2e
Sammy Wu <swucode@gmail.com>
2018-05-16 05:18:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+//see all passengers related to driver//
+//driver become srleated to passenger when he takes a single trip//
+//one passenger associated with trip//
+
+let driverId = 0;
+
+let store = {drivers: [], passengers: [], trips: []};
+
+class Driver {
+  constructor(name) {
+    this.id = ++driverId;
+    this.name = name;
+    store.drivers.push(this)
+  }
+  trips() {
+      return store.trips.filter(trip => trip.driverId === this.id)
+  }
+  passengers() {
+    return this.trips().map((trip) => trip.passenger());
+  }
+}
+
+class Passenger {
+  
+}
+
+class Trip {
+  
+}
+
